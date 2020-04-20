bbbe7a7f64607df0c83f70bb89cff11f3bbaf3c3
EV-Smith <emilie.v.smi@gmail.com>
2020-04-19 20:59:26 -0700

    Basic landing html and css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className="bg-rust text-white mt-5 p-4 text-center">
+    Copyright &copy; {new Date().getFullYear()} Photo App
+  </footer>
   )
 }
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 
-export default class Landing extends Component {
+class Landing extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Photo App
+                </h1>
+                <p className="lead"> Share your photos with the world!</p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
+                <a href="login.html" className="btn btn-lg btn-light">Login</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+export default Landing
